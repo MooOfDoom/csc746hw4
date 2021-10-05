d39ccd47775053103d3bf9ac0f53a6db4fb259a3
MooOfDoom <lotharnarins@hotmail.com>
2021-10-04 20:21:18 -0700

    Add std:: to align

--- benchmark.cpp
+++ benchmark.cpp
@@ -203,7 +203,7 @@ int main(int argc, char** argv)
 
          // allocate memory for 6 NxN matrics
          std::vector<double> buf(6 * n * n + 8); // 8 extra doubles for alignment
-         double* A = (double *)align(64, sizeof(double), buf.data(), sizeof(double) * (6 * n * n + 8));
+         double* A = (double *)std::align(64, sizeof(double), buf.data(), sizeof(double) * (6 * n * n + 8));
          double* B = A + n * n;
          double* C = B + n * n;
          double* Acopy = C + n * n;
